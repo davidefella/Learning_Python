810226ebabbf3b7fed71f363e379ddc1698529d8
Davide Fella <davide.fella@gmail.com>
2020-05-02 15:18:11 +0200

    Multiply params

---
+++ src/multiply.py
@@ -0,0 +1,12 @@
+"""
+
+Write a function that will receive 2 numbers as input and it should return the multiplication of these 2 numbers.
+
+Input: Two arguments. Both are int
+
+Output: Int.
+"""
+def multiplyTwoNumbers(x, y):
+  return x * y
+
+print(multiplyTwoNumbers(2,5))
